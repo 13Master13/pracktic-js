f9c36a58dd4c1a4a5b8ce7d8fef877ee48fdbaf0
13Master13 <141366261+13Master13@users.noreply.github.com>
2023-11-18 08:33:28 +0200

    Settings

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let mas = console.log('Hello') //Простой вывод в консоль
+let mas_1 = alert('Hello 1') //Простое модальное окно
+let mas_2 = confirm('Hello 2?') //Модальное окно "Да/Нет". Возвращает "true/false"
+let mas_3 = prompt('Hello 3') //Модальное окно с введением текста. Возвращает "string/null"
